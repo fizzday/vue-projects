857a2b8d2cae7445103a1db8077665d38dcf5118
fizzday <fizzday@yeah.net>
2018-04-16 18:12:02 +0800

    api

--- src/util/preRequests.js
+++ src/util/preRequests.js
@@ -4,7 +4,7 @@ import qs from 'qs'
 axios.defaults.timeout = 15000;
 // axios.defaults.headers["Content-Type"] = 'application/x-www-form-urlencoded;charset=UTF-8';
 axios.defaults.baseURL = 'http://127.0.0.1:5000';//api通用前缀设置
-// axios.defaults.baseURL = 'http://fizzday.net/api';//api通用前缀设置
+axios.defaults.baseURL = 'http://fizzday.net/api';//api通用前缀设置
 // axios.defaults.baseURL = 'https://api.simboss.com';//api通用前缀设置
 // axios.defaults.baseURL = 'http://mobile_card.cc';//api通用前缀设置
 
